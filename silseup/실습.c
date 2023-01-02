4d33be962331db34c2b448c8a3398d28512f9ba9
Park ChanBeen <cb18277@naver.com>
2023-01-03 00:48:06 +0900

    sadf

--- silseup/실습.c
+++ silseup/실습.c
@@ -2,15 +2,6 @@
 #pragma warning(disable:4996)
 
 int main() {
-
-	float jj = 35;
-	float yj = 15;
-	float ct = 20;
-	float leader = 50;
-
-
-	printf("%f\n", yj * 0.3);
-	float res = jj * 0.3 + yj * 0.3 + ct * 0.2 + leader * 0.2;
-
-	printf("%f", res);
+	printf("hello world");
+	return 0;
 }
